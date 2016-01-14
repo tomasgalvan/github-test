806291c0a4169a4d18d59baf4531116905b6c598
Tomasgalvan <galvan.t@gmail.com>
2016-01-13 20:40:17 -0800

    second day stuff

---
+++ Day 2/app.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your First Name?");
+var lastName = prompt("What is your Last Name?");
+console.log (firstName)
+alert (lastName)
+var choice = confirm ("Do you smell what the rock is cooking?")
+alert (choice)
---
+++ Day 2/myjsfile.js
@@ -0,0 +1,9 @@
+//tell the user something
+alert("Ah, hi there, I'll wait here till you close me");
+
+//confirm a user’s action
+var userChoice = confirm("Sure you want to do that...?");
+console.log(userChoice);
+//ask the user for input
+var username = prompt("What is your name?");
+console.log(username);
