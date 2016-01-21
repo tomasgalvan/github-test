506a373025af82b6ece882506586b035c6c90c0f
Tomasgalvan <galvan.t@gmail.com>
2016-01-20 16:44:03 -0800

    assignment day 3

---
+++ Day 3/Notes/Assignment/assignment.js
@@ -0,0 +1,11 @@
+var userPrompt = prompt("Please enter your name");
+
+if( (userPrompt == null) || (userPrompt == "") )
+{
+	alert("enter your name PLEASE!!!");
+}
+else 
+{
+alert(userPrompt);
+console.log(userPrompt);
+}
